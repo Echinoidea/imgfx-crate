e2a8ddbaea651def11ef290a2c582679e8636ff8
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2025-09-06 11:56:07 -0400

    fixed u8 under/overflow

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -2,11 +2,24 @@ use crate::utils::{get_channel_by_name_rgb_color, get_channel_by_name_rgba_u8};
 use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb, Rgba, RgbaImage};
 use rayon::prelude::*;
 
+/// Add blend mode operation.
+/// Adds the input color RGB to each pixel's RGB. Overflows.
+/// RGB channels are remappable before operation.
+/// * `img` - The image::DynamicImage input to perform the operation on.
+/// * `lhs` - Optional vector of Strings to remap the order of the channels of the left-hand side.
+/// * `rhs` - Optional vector of Strings to remap the order of the channels of the right-hand side.
+/// * `color` - Temporarily based on the Clap RgbColor struct. The right-hand side of the operation.
+/// * `raw` - bool, if true, allow for u8 overflow, else, clamp at 255.
 pub fn add(
     img: DynamicImage,
     lhs: Option<Vec<String>>,
     rhs: Option<Vec<String>>,
+<<<<<<< HEAD
     color: Rgb<u8>,
+=======
+    color: RgbColor,
+    raw: bool,
+>>>>>>> 29018c7 (fixed u8 under/overflow)
 ) -> RgbaImage {
     let r = color.0[0];
     let g = color.0[1];
@@ -37,17 +50,38 @@ pub fn add(
             None => (in_pixel[0], in_pixel[1], in_pixel[2]),
         };
 
-        let r = lhs.0 + rhs.0;
-        let g = lhs.1 + rhs.1;
-        let b = lhs.2 + rhs.2;
-        let a = in_pixel[3];
+        let (r, g, b) = match raw {
+            true => {
+                let r = lhs.0 + rhs.0;
+                let g = lhs.1 + rhs.1;
+                let b = lhs.2 + rhs.2;
 
+                (r, g, b)
+            }
+            false => {
+                let r = (lhs.0 as i32 + rhs.0 as i32).min(255) as u8;
+                let g = (lhs.1 as i32 + rhs.1 as i32).min(255) as u8;
+                let b = (lhs.2 as i32 + rhs.2 as i32).min(255) as u8;
+
+                (r, g, b)
+            }
+        };
+
+        let a = in_pixel[3];
         *pixel = Rgba([r, g, b, a]);
     });
 
     output
 }
 
+/// Subtraction blend mode operation.
+/// Subtracts the input color RGB from each pixel's RGB.
+/// RGB channels are remappable before operation.
+/// * `img` - The image::DynamicImage input to perform the operation on.
+/// * `lhs` - Optional vector of Strings to remap the order of the channels of the left-hand side.
+/// * `rhs` - Optional vector of Strings to remap the order of the channels of the right-hand side.
+/// * `color` - Temporarily based on the Clap RgbColor struct. The right-hand side of the operation.
+/// * `raw` - bool, if true, allow for u8 underflow, else, get the absolute value of the operation.
 pub fn sub(
     img: DynamicImage,
     lhs: Option<Vec<String>>,
@@ -108,6 +142,18 @@ pub fn sub(
     output
 }
 
+/// Multiplication blend mode operation.
+/// Multiplies each pixel's RGB by the RGB of the color param. Overflows.
+///
+/// This operation often results in images with very extreme color noise and as such will
+/// take up significantly more storage than the inputted image. Sometimes up to 36 times larger
+/// outputs. It doesn't matter what encoder is used.
+///
+/// RGB channels are remappable before operation.
+/// * `img` - The image::DynamicImage input to perform the operation on.
+/// * `lhs` - Optional vector of Strings to remap the order of the channels of the left-hand side.
+/// * `rhs` - Optional vector of Strings to remap the order of the channels of the right-hand side.
+/// * `color` - Temporarily based on the Clap RgbColor struct. The right-hand side of the operation.
 pub fn mult(
     img: DynamicImage,
     lhs: Option<Vec<String>>,
@@ -154,6 +200,19 @@ pub fn mult(
     output
 }
 
+/// Exponential blend mode operation.
+/// Calculates the power of each channel where the image pixel RGB is the base and the color param
+/// RGB is the exponent. Overflows.
+///
+/// This operation often results in images with very extreme color noise and as such will
+/// take up significantly more storage than the inputted image. Sometimes up to 36 times larger
+/// outputs. It doesn't matter what encoder is used.
+///
+/// RGB channels are remappable before operation.
+/// * `img` - The image::DynamicImage input to perform the operation on.
+/// * `lhs` - Optional vector of Strings to remap the order of the channels of the left-hand side.
+/// * `rhs` - Optional vector of Strings to remap the order of the channels of the right-hand side.
+/// * `color` - Temporarily based on the Clap RgbColor struct. The right-hand side of the operation.
 pub fn pow(
     img: DynamicImage,
     lhs: Option<Vec<String>>,
@@ -201,6 +260,17 @@ pub fn pow(
     output
 }
 
+/// Division blend mode operation.
+/// Divides the image pixel RGB by the color param RGB.
+///
+/// This operation is quite useful for isolating or removing particular colors when used with the
+/// lhs remapping.
+///
+/// RGB channels are remappable before operation.
+/// * `img` - The image::DynamicImage input to perform the operation on.
+/// * `lhs` - Optional vector of Strings to remap the order of the channels of the left-hand side.
+/// * `rhs` - Optional vector of Strings to remap the order of the channels of the right-hand side.
+/// * `color` - Temporarily based on the Clap RgbColor struct. The right-hand side of the operation.
 pub fn div(
     img: DynamicImage,
     lhs: Option<Vec<String>>,
@@ -278,7 +348,11 @@ mod tests {
         let red = load_image("ff0000.png".to_string());
         let control_color = get_color_from_control(red.clone());
 
+<<<<<<< HEAD
         let out = add(red.clone(), None, None, Rgb([0, 0, 255]));
+=======
+        let out = add(red.clone(), None, None, RgbColor(0, 0, 255), true);
+>>>>>>> 29018c7 (fixed u8 under/overflow)
 
         println!(
             "{:?} == {:?}",
